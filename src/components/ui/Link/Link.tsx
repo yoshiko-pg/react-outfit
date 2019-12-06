545c5e7149a26b6f17e7900942040f9e8f9ebd0d
maasa <maasa.giss2@gmail.com>
2019-12-06 18:14:40 +0900

    tmp

--- src/components/ui/Link/Link.tsx
+++ src/components/ui/Link/Link.tsx
@@ -1,46 +1,50 @@
-import React, { PropsWithoutRef } from 'react'
+import React from 'react'
 import cx from 'classnames'
 import { Link as RouterLink } from 'react-router-dom'
 
 import styles from './Link.module.css'
 
-type Props = PropsWithoutRef<JSX.IntrinsicElements['a']> & {
+type LinkProps = React.ComponentProps<'a'> & {
   children?: React.ReactNode
   className?: string
   disabled?: boolean
-  href?: string
+  href: string
   push?: boolean
   withoutStyle?: boolean
 }
 
-const Link = ({
-  children,
-  className,
-  disabled,
-  href,
-  push = false,
-  withoutStyle = false,
-  ...props
-}: Props) => {
+type ButtonProps = React.ComponentProps<'button'> & {
+  children?: React.ReactNode
+  className?: string
+  withoutStyle?: boolean
+}
+
+const Link = (props: LinkProps | ButtonProps) => {
+  const { children, className, disabled, withoutStyle = false, ...rest } = props
+
   const allClassName = cx(
     styles.reset,
     { [styles.link]: !withoutStyle, [styles.disabled]: disabled },
     className,
   )
 
+  // hrefがなかったらBUttonPropsになる
+
   // button
   if (!href) {
     return (
-      <button className={allClassName} disabled={disabled} type="button" role="link" {...props}>
+      <button className={allClassName} disabled={disabled} type="button" role="link" {...rest}>
         {children}
       </button>
     )
   }
 
+  const { href, push = false } = props
+
   // disabled link
   if (disabled) {
     return (
-      <span className={allClassName} {...props}>
+      <span className={allClassName} {...rest}>
         {children}
       </span>
     )
@@ -49,7 +53,7 @@ const Link = ({
   // push link
   if (push) {
     return (
-      <RouterLink className={allClassName} to={href} {...props}>
+      <RouterLink className={allClassName} to={href} {...rest}>
         {children}
       </RouterLink>
     )
@@ -57,7 +61,7 @@ const Link = ({
 
   // link
   return (
-    <a className={allClassName} href={href} {...props}>
+    <a className={allClassName} href={href} {...rest}>
       {children}
     </a>
   )
